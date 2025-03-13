cc3283257227beecc7870629a9e951d2626acc0c
temathugga <lowskilldoter@mail.ru>
2025-03-13 15:44:32 +0300

    Create vartype.py

---
+++ vartype.py
@@ -0,0 +1,15 @@
+# Программа 1
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+summa = number1 + number2
+print(f"Сумма чисел равна: {summa}")
+
+# Программа 2
+user_input = input("Введите число: ")  
+number = int(user_input)
+print(f"Вы ввели число: {number}")
+print(f"Тип данных введенного числа: {type(number)}")
+
+# Программа 3
+user_name = input("Введите ваше имя: ")
+print(f"Привет, {user_name}!")
