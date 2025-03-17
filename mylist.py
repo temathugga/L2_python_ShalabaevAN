38f6fd0e34bd59c52e29886b43c51649ea5184e6
temathugga <lowskilldoter@mail.ru>
2025-03-17 12:10:23 +0300

    Create mylist.py

---
+++ mylist.py
@@ -0,0 +1,14 @@
+# Программа 1
+numbers = [10, 20, 30, 40, 50]
+sum_of_numbers = sum(numbers)
+print("Сумма чисел в списке:", sum_of_numbers)
+
+# Программа 2
+numbers = [15, 7, 42, 3, 56]
+max_number = max(numbers)
+print("Максимальное число в списке:", max_number)
+
+# Программа 3
+numbers = [1, 2, 2, 3, 4, 4, 5, 5, 5]
+unique_numbers = list(set(numbers))
+print("Список без дубликатов:", unique_numbers)
