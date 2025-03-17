1b4373becb6b9cf69b3432bfae07a04e48e0b5ed
temathugga <lowskilldoter@mail.ru>
2025-03-17 12:07:22 +0300

    Create forwhile.py

---
+++ forwhile.py
@@ -0,0 +1,16 @@
+# Программа 1
+for i in range(1, 11):
+    print(i)
+
+# Программа 2
+N = int(input("Введите число N: "))
+for i in range(1, N + 1):
+    print(i)
+
+# Программа 3
+sum = 0
+i = 1
+while i <= 100:
+    sum += i
+    i += 1
+print("Сумма чисел от 1 до 100:", sum)
